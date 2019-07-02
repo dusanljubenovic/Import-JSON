5ead3117beabdea6f094cf5af8be64607d6e1575
Dusan <dusan@buddyboss.com>
2019-07-02 15:29:39 +0200

    Change name of variable

--- twentynineteen/test/script.php
+++ twentynineteen/test/script.php
@@ -1,23 +1,23 @@
 <?php
 
 
-function import_developer() {
+function import_events() {
 
-	$developer_data = json_decode( file_get_contents( get_template_directory_uri().'/test/datas.json' ) );
+	$events_data = json_decode( file_get_contents( get_template_directory_uri().'/test/datas.json' ) );
 
-  insert_or_update( $developer_data );
+  insert_or_update( $events_data );
 
   wp_die();
   
   }
 
-  function insert_or_update($developer_data) {
+  function insert_or_update($events_data) {
 
 
-	if ( ! $developer_data)
+	if ( ! $events_data)
 	  return false;
      
-    foreach($developer_data as $event){
+    foreach($events_data as $event){
 
        $number_updates=0;
        $number_new_events=0;
@@ -139,4 +139,4 @@ function import_developer() {
   
   }
 
-	import_developer();
+	import_events();
